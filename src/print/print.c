e2f18fca7200b731ada5769e119f47d2ece930d6
Joel van Ingen <j.c.ingen@gmail.com>
2025-02-24 23:44:28 +0100

    (buggy) first implementation of lexer and parser

--- src/print/print.c
+++ src/print/print.c
@@ -9,18 +9,40 @@
 
 #include "ccn/ccn.h"
 #include "ccngen/ast.h"
+#include "ccngen/enum.h"
 #include "ccngen/trav.h"
 #include "palm/dbug.h"
 
-void printArithCounts(node_st *node) {
-    int add = PROGRAM_SUM_ADD(node);
-    int sub = PROGRAM_SUM_SUB(node);
-    int mul = PROGRAM_SUM_MUL(node);
-    int div = PROGRAM_SUM_DIV(node);
-    int mod = PROGRAM_SUM_MOD(node);
+static int INDENT = 0;
 
-    printf("+: %i\n-: %i\n*: %i\n/: %i\n%%: %i\n",
-        add, sub, mul, div, mod);
+static char *TRUE_STRING = "true";
+static char *FALSE_STRING = "false";
+
+static char *BOOL_STRING = "bool";
+static char *INT_STRING = "int";
+static char *FLOAT_STRING = "float";
+static char *VOID_STRING = "void";
+
+static char *BINOP_OP_STRINGS[] = {"", "+", "-", "*", "/", "%", "<", "<=", ">", ">=", "==", "!=", "&&", "||"};
+
+static char *bool_to_string(const bool b) {
+    return b ? TRUE_STRING : FALSE_STRING;
+}
+
+static char *type_to_string(const enum Type type) {
+    switch(type) {
+        case CT_bool: return BOOL_STRING;
+        case CT_int: return INT_STRING;
+        case CT_float: return FLOAT_STRING;
+        case CT_void: return VOID_STRING;
+        default: fprintf(stderr, "UNKNOWN TYPE"); return NULL;
+    }
+}
+
+static void print_indent() {
+    for(int i = 0; i < INDENT; i++) {
+        printf("\t");
+    }
 }
 
 /**
@@ -28,8 +50,214 @@ void printArithCounts(node_st *node) {
  */
 node_st *PRTprogram(node_st *node)
 {
-    TRAVstmts(node);
-    printArithCounts(node);
+    printf("START OF PROGRAM\n");
+    TRAVchildren(node);
+    printf("\nEND OF PROGRAM\n");
+    return node;
+}
+
+/**
+ * @fn PRTdecls
+ */
+node_st *PRTdecls(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTexprs
+ */
+node_st *PRTexprs(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTarrexpr
+ */
+node_st *PRTarrexpr(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTids
+ */
+node_st *PRTids(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTexprstmt
+ */
+node_st *PRTexprstmt(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTreturn
+ */
+node_st *PRTreturn(node_st *node)
+{
+    printf("RETURN(");
+    TRAVchildren(node);
+    printf(")");
+    return node;
+}
+
+/**
+ * @fn PRTfuncall
+ */
+node_st *PRTfuncall(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTcast
+ */
+node_st *PRTcast(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTfundefs
+ */
+node_st *PRTfundefs(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTfundef
+ */
+node_st *PRTfundef(node_st *node)
+{
+    bool has_body = FUNDEF_BODY(node) != NULL;
+
+    print_indent();
+
+    if (has_body) printf("BEGIN ");
+
+    printf("FUNDEF(name=%s, type=%s",
+        FUNDEF_NAME(node), type_to_string(FUNDEF_TYPE(node)));
+    
+    // Print args if they exist
+    if (FUNDEF_PARAMS(node) != NULL) {
+        printf(", params=(");
+        TRAVparams(node);
+    }
+
+    printf(")");
+    
+    if (has_body) {
+        INDENT++;
+        TRAVchildren(node);
+        INDENT--;
+        printf("\nEND FUNDEF(name=%s)", FUNDEF_NAME(node));
+    }
+    
+    
+    return node;
+}
+
+/**
+ * @fn PRTfunbody
+ */
+node_st *PRTfunbody(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTifelse
+ */
+node_st *PRTifelse(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTwhile
+ */
+node_st *PRTwhile(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTdowhile
+ */
+node_st *PRTdowhile(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTfor
+ */
+node_st *PRTfor(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTglobdecl
+ */
+node_st *PRTglobdecl(node_st *node)  // TODO: ADD DIMS
+{
+    printf("GLOBDECL(name=%s, type=%s)",
+        GLOBDECL_NAME(node), type_to_string(GLOBDECL_TYPE(node)));
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTglobdef
+ */
+node_st *PRTglobdef(node_st *node)  // TODO: ADD DIMS, INIT
+{
+    printf("GLOBDEF(export=%s name=%s, type=%s)",
+        bool_to_string(GLOBDEF_EXPORT(node)), GLOBDEF_NAME(node), type_to_string(GLOBDECL_TYPE(node)));
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTparam
+ */
+node_st *PRTparam(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn PRTvardecl
+ */
+node_st *PRTvardecl(node_st *node)
+{
+    printf("\n");
+    print_indent();
+    printf("VARDECL(name=%s, type=%s)",
+        VARDECL_NAME(node), type_to_string(VARDECL_TYPE(node)));
+    TRAVchildren(node);
     return node;
 }
 
@@ -38,8 +266,9 @@ node_st *PRTprogram(node_st *node)
  */
 node_st *PRTstmts(node_st *node)
 {
-    TRAVstmt(node);
-    TRAVnext(node);
+    printf("\n");
+    print_indent();
+    TRAVchildren(node);
     return node;
 }
 
@@ -48,16 +277,11 @@ node_st *PRTstmts(node_st *node)
  */
 node_st *PRTassign(node_st *node)
 {
-
-    if (ASSIGN_LET(node) != NULL) {
-        TRAVlet(node);
-        printf( " = ");
-    }
-
+    printf("ASSIGN(");
+    TRAVlet(node);
+    printf(" <- ");
     TRAVexpr(node);
-    printf( ";\n");
-
-
+    printf(")");
     return node;
 }
 
@@ -66,61 +290,20 @@ node_st *PRTassign(node_st *node)
  */
 node_st *PRTbinop(node_st *node)
 {
-    char *tmp = NULL;
-    printf( "( ");
-
+    printf("BINOP(");
     TRAVleft(node);
-
-    switch (BINOP_OP(node)) {
-    case BO_add:
-      tmp = "+";
-      break;
-    case BO_sub:
-      tmp = "-";
-      break;
-    case BO_mul:
-      tmp = "*";
-      break;
-    case BO_div:
-      tmp = "/";
-      break;
-    case BO_mod:
-      tmp = "%";
-      break;
-    case BO_lt:
-      tmp = "<";
-      break;
-    case BO_le:
-      tmp = "<=";
-      break;
-    case BO_gt:
-      tmp = ">";
-      break;
-    case BO_ge:
-      tmp = ">=";
-      break;
-    case BO_eq:
-      tmp = "==";
-      break;
-    case BO_ne:
-      tmp = "!=";
-      break;
-    case BO_or:
-      tmp = "||";
-      break;
-    case BO_and:
-      tmp = "&&";
-      break;
-    case BO_NULL:
-      DBUG_ASSERT(false, "unknown binop detected!");
-    }
-
-    printf( " %s ", tmp);
-
+    printf(" %s ", BINOP_OP_STRINGS[BINOP_OP(node)]);
     TRAVright(node);
+    printf(")");
+    return node;
+}
 
-    printf( ")(%d:%d-%d)", NODE_BLINE(node), NODE_BCOL(node), NODE_ECOL(node));
-
+/**
+ * @fn PRTmonop
+ */
+node_st *PRTmonop(node_st *node)
+{
+    TRAVchildren(node);
     return node;
 }
 
@@ -129,7 +312,8 @@ node_st *PRTbinop(node_st *node)
  */
 node_st *PRTvarlet(node_st *node)
 {
-    printf("%s(%d:%d)", VARLET_NAME(node), NODE_BLINE(node), NODE_BCOL(node));
+    printf("VARLET(%s)", VARLET_NAME(node));
+    TRAVchildren(node);
     return node;
 }
 
@@ -138,7 +322,8 @@ node_st *PRTvarlet(node_st *node)
  */
 node_st *PRTvar(node_st *node)
 {
-    printf( "%s", VAR_NAME(node));
+    printf("VAR(%s)", VAR_NAME(node));
+    TRAVchildren(node);
     return node;
 }
 
@@ -147,7 +332,8 @@ node_st *PRTvar(node_st *node)
  */
 node_st *PRTnum(node_st *node)
 {
-    printf("%d", NUM_VAL(node));
+    printf("NUM(%i)", NUM_VAL(node));
+    TRAVchildren(node);
     return node;
 }
 
@@ -156,7 +342,8 @@ node_st *PRTnum(node_st *node)
  */
 node_st *PRTfloat(node_st *node)
 {
-    printf( "%f", FLOAT_VAL(node));
+    printf("FLOAT(%f)", FLOAT_VAL(node));
+    TRAVchildren(node);
     return node;
 }
 
@@ -165,7 +352,135 @@ node_st *PRTfloat(node_st *node)
  */
 node_st *PRTbool(node_st *node)
 {
-    char *bool_str = BOOL_VAL(node) ? "true" : "false";
-    printf("%s", bool_str);
+    printf("BOOL(%s)", bool_to_string(BOOL_VAL(node)));
+    TRAVchildren(node);
     return node;
 }
+
+//
+// /**
+//  * @fn PRTassign
+//  */
+// node_st *PRTassign(node_st *node)
+// {
+//
+//     if (ASSIGN_LET(node) != NULL) {
+//         TRAVlet(node);
+//         printf( " = ");
+//     }
+//
+//     TRAVexpr(node);
+//     printf( ";\n");
+//
+//
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTbinop
+//  */
+// node_st *PRTbinop(node_st *node)
+// {
+//     char *tmp = NULL;
+//     printf( "( ");
+//
+//     TRAVleft(node);
+//
+//     switch (BINOP_OP(node)) {
+//     case BO_add:
+//       tmp = "+";
+//       break;
+//     case BO_sub:
+//       tmp = "-";
+//       break;
+//     case BO_mul:
+//       tmp = "*";
+//       break;
+//     case BO_div:
+//       tmp = "/";
+//       break;
+//     case BO_mod:
+//       tmp = "%";
+//       break;
+//     case BO_lt:
+//       tmp = "<";
+//       break;
+//     case BO_le:
+//       tmp = "<=";
+//       break;
+//     case BO_gt:
+//       tmp = ">";
+//       break;
+//     case BO_ge:
+//       tmp = ">=";
+//       break;
+//     case BO_eq:
+//       tmp = "==";
+//       break;
+//     case BO_ne:
+//       tmp = "!=";
+//       break;
+//     case BO_or:
+//       tmp = "||";
+//       break;
+//     case BO_and:
+//       tmp = "&&";
+//       break;
+//     case BO_NULL:
+//       DBUG_ASSERT(false, "unknown binop detected!");
+//     }
+//
+//     printf( " %s ", tmp);
+//
+//     TRAVright(node);
+//
+//     printf( ")(%d:%d-%d)", NODE_BLINE(node), NODE_BCOL(node), NODE_ECOL(node));
+//
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTvarlet
+//  */
+// node_st *PRTvarlet(node_st *node)
+// {
+//     printf("%s(%d:%d)", VARLET_NAME(node), NODE_BLINE(node), NODE_BCOL(node));
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTvar
+//  */
+// node_st *PRTvar(node_st *node)
+// {
+//     printf( "%s", VAR_NAME(node));
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTnum
+//  */
+// node_st *PRTnum(node_st *node)
+// {
+//     printf("%d", NUM_VAL(node));
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTfloat
+//  */
+// node_st *PRTfloat(node_st *node)
+// {
+//     printf( "%f", FLOAT_VAL(node));
+//     return node;
+// }
+//
+// /**
+//  * @fn PRTbool
+//  */
+// node_st *PRTbool(node_st *node)
+// {
+//     char *bool_str = BOOL_VAL(node) ? "true" : "false";
+//     printf("%s", bool_str);
+//     return node;
+// }
--- src/scanparse/parser.y
+++ src/scanparse/parser.y
@@ -25,33 +25,102 @@ void AddLocToNode(node_st *node, void *begin_loc, void *end_loc);
  char               *id;
  int                 cint;
  float               cflt;
+ bool               cbool;
+ enum MonOpType     cmonop;
  enum BinOpType     cbinop;
+ enum Type          ctype;
  node_st             *node;
 }
 
 %locations
 
-%token BRACKET_L BRACKET_R COMMA SEMICOLON
+%token BRACE_L BRACE_R BRACKET_L BRACKET_R SBRACKET_L SBRACKET_R COMMA SEMICOLON
+%token IF ELSE WHILE DO FOR RETURN
 %token MINUS PLUS STAR SLASH PERCENT LE LT GE GT EQ NE OR AND
+%token NOT
 %token TRUEVAL FALSEVAL LET
 
 %token <cint> NUM
 %token <cflt> FLOAT
 %token <id> ID
+%token INTTYPE FLOATTYPE BOOLTYPE VOIDTYPE
+%token EXPORT EXTERN
 
-%type <node> intval floatval boolval constant expr
-%type <node> stmts stmt assign varlet program
+%type <node> program decls expr exprs arrexpr ids exprstmt
+%type <node> returnstmt funcall cast fundefs fundef funbody
+%type <node> ifstmt whilestmt dowhilestmt forstmt
+%type <node> globdecl globdef param vardecl stmts
+%type <node> assign varlet var
+%type <node> intval floatval boolval
+%type <cmonop> monop
 %type <cbinop> binop
+%type <ctype> type
+%type <node> constants operations decl stmt
+
+%type <node> opt_vardecl opt_fundefs opt_stmts opt_exprs
+%type <node> opt_params param_list opt_funbody opt_ids opt_dims
+%type <cbool> opt_export_bool
+
+// Precedence rules
+%left OR AND
+%left EQ NE
+%left LT LE GT GE
+%left PLUS MINUS
+%left STAR SLASH PERCENT
+%right NOT
+%right UMINUS
 
 %start program
 
 %%
 
-program: stmts
+program: decls
          {
            parseresult = ASTprogram($1);
          }
          ;
+
+decls:  decl decls
+        {
+          $$ = ASTdecls($1, $2);
+        }
+      | decl
+        {
+          $$ = ASTdecls($1, NULL);
+        }
+        ;
+
+decl: globdecl { $$ = $1; }
+    | globdef { $$ = $1; }
+    | fundef { $$ = $1; }
+    ;
+
+globdecl: EXTERN type[t] ids[dims] ID[name] SEMICOLON
+          {
+            $$ = ASTglobdecl($name, $t);
+            GLOBDECL_DIMS($$) = $dims;
+            AddLocToNode($$, &@t, &@name);
+          }
+        | EXTERN type[t] ID[name] SEMICOLON
+          {
+            $$ = ASTglobdecl($name, $t);
+            AddLocToNode($$, &@t, &@name);
+          }
+          ;
+
+globdef: opt_export_bool[export] type[t] opt_ids[dims] ID[name] LET expr[init] SEMICOLON
+        {
+          $$ = ASTglobdef($name, $t);
+          GLOBDEF_DIMS($$) = $dims;
+          GLOBDEF_INIT($$) = $init;
+          GLOBDEF_EXPORT($$) = $export;
+          AddLocToNode($$, &@t, &@init);
+        }
+        ;
+
+opt_export_bool: EXPORT { $$ = true; } | /* Empty */ { $$ = false; };
+
+opt_ids: ids { $$ = $1; } | /* Empty */ { $$ = NULL; };
 
 stmts: stmt stmts
         {
@@ -63,46 +132,227 @@ stmts: stmt stmts
         }
         ;
 
-stmt: assign
-       {
-         $$ = $1;
-       }
-       ;
+stmt:   assign { $$ = $1; }
+      | exprstmt { $$ = $1; }
+      | ifstmt { $$ = $1; }
+      | whilestmt { $$ = $1; }
+      | dowhilestmt { $$ = $1; }
+      | forstmt { $$ = $1; }
+      | returnstmt { $$ = $1; }
+      ;
+
+opt_params: param_list { $$ = $1; } | /* Empty */ { $$ = NULL; };
+
+param_list: param { $$ = $1; }
+          | param_list COMMA param
+            {
+              $$ = $1;
+              PARAM_NEXT($1) = $3;
+            }
+            ;
+
+opt_funbody: funbody { $$ = $1; } | /* Empty */ { $$ = NULL; };
+
+fundef: opt_export_bool[export] type[t] ID[name] BRACKET_L opt_params[params] BRACKET_R opt_funbody[body]
+        {
+          $$ = ASTfundef($name, $t);
+          FUNDEF_PARAMS($$) = $params;
+          FUNDEF_BODY($$) = $body;
+          FUNDEF_EXPORT($$) = $export;
+          AddLocToNode($$, &@t, &@name);
+        }
+        ;
+
+fundefs: fundef fundefs
+         {
+          $$ = ASTfundefs($1);
+          FUNDEFS_NEXT($$) = $2;
+         }
+       | fundef
+         {
+          $$ = ASTfundefs($1);
+         }
+         ;
+
+opt_vardecl: vardecl { $$ = $1; } | /* Empty */ { $$ = NULL; };
+opt_fundefs: fundefs { $$ = $1; } | /* Empty */ { $$ = NULL; };
+// opt_stmts: stmts { $$ = $1; } | /* Empty */ { $$ = NULL; };
+
+funbody: BRACE_L[startbrace] opt_vardecl[decls] opt_fundefs[fundefs] stmts[stmts] BRACE_R[endbrace]
+          {
+            $$ = ASTfunbody();
+            FUNBODY_DECLS($$) = $decls;
+            FUNBODY_LOCAL_FUNDEFS($$) = $fundefs;
+            FUNBODY_STMTS($$) = $stmts;
+            AddLocToNode($$, &@startbrace, &@endbrace);
+          }
+          ;
+
+param: type[t] ID[id] ids[dims]
+      {
+        $$ = ASTparam($id, $t);
+        PARAM_DIMS($$) = $dims;
+        AddLocToNode($$, &@t, &@id);
+      }
+      ;
 
 assign: varlet LET expr SEMICOLON
         {
           $$ = ASTassign($1, $3);
+          AddLocToNode($$, &@1, &@3);
         }
         ;
 
-varlet: ID
+exprstmt: expr SEMICOLON { $$ = ASTexprstmt($1); };
+
+ifstmt: IF BRACKET_L expr[cond] BRACKET_R BRACE_L stmts[then] BRACE_R ELSE BRACE_L stmts[else] BRACE_R
         {
-          $$ = ASTvarlet($1);
+          $$ = ASTifelse($cond);
+          IFELSE_THEN($$) = $then;
+          IFELSE_ELSE_BLOCK($$) = $else;
+          AddLocToNode($$, &@1, &@else);
+        }
+      | IF BRACKET_L expr[cond] BRACKET_R BRACE_L stmts[then] BRACE_R
+        {
+          $$ = ASTifelse($cond);
+          IFELSE_THEN($$) = $then;
+          AddLocToNode($$, &@1, &@then);
+        }
+        ;
+
+whilestmt: WHILE BRACKET_L expr[cond] BRACKET_R BRACE_L stmts[block] BRACE_R
+            {
+              $$ = ASTwhile($cond);
+              WHILE_BLOCK($$) = $block;
+              AddLocToNode($$, &@1, &@block);
+            }
+          ;
+
+dowhilestmt: DO BRACE_L stmts[block] BRACE_R WHILE BRACKET_L expr[cond] BRACKET_R
+            {
+              $$ = ASTdowhile($cond);
+              DOWHILE_BLOCK($$) = $block;
+            }
+          ;
+
+forstmt: FOR BRACKET_L ID[var] LET expr[init] COMMA expr[stop] COMMA expr[step] BRACKET_R BRACE_L stmts[block] BRACE_R
+           {
+             $$ = ASTfor($init, $stop);
+             FOR_STEP($$) = $step;
+             FOR_BLOCK($$) = $block;
+             FOR_VAR($$) = $var;
+           }
+         | FOR BRACKET_L ID[var] LET expr[init] COMMA expr[stop] BRACKET_R BRACE_L stmts[block] BRACE_R
+           {
+             $$ = ASTfor($init, $stop);
+             FOR_BLOCK($$) = $block;
+             FOR_VAR($$) = $var;
+           }
+         ;
+
+returnstmt: RETURN expr[val] SEMICOLON
+            {
+              $$ = ASTreturn();
+              RETURN_EXPR($$) = $val;
+            }
+          | RETURN SEMICOLON
+            {
+              $$ = ASTreturn();
+            }
+          ;
+
+funcall: ID[name] BRACKET_L exprs[args] BRACKET_R
+        {
+          $$ = ASTfuncall($name);
+          FUNCALL_FUN_ARGS($$) = $args;
+        }
+        ;
+
+ids: ID
+      {
+        $$ = ASTids($1);
+      }
+   | ID COMMA ids
+      {
+        $$ = ASTids($1);
+        IDS_NEXT($$) = $3;
+      }
+      ;
+
+opt_exprs: exprs { $$ = $1; } | /* Empty */ { $$ = NULL; };
+
+vardecl: type[t] ID[name] SEMICOLON  // TODO: ADD DIMS
+        {
+          $$ = ASTvardecl($name, $t);
+          // VARDECL_DIMS($$) = $dims;
+        }
+       | type[t] ID[name] LET expr[init] SEMICOLON
+        {
+          $$ = ASTvardecl($name, $t);
+          VARDECL_INIT($$) = $init;
+          // VARDECL_DIMS($$) = $dims;
+        }
+
+varlet: ID[id] SBRACKET_L exprs[indices] SBRACKET_R
+        {
+          $$ = ASTvarlet($id);
+          VARLET_INDICES($$) = $indices;
+          AddLocToNode($$, &@1, &@1);
+        }
+      | ID[id]
+        {
+          $$ = ASTvarlet($id);
           AddLocToNode($$, &@1, &@1);
         }
         ;
 
+var: ID[id] SBRACKET_L exprs[indices] SBRACKET_R
+        {
+          $$ = ASTvar($id);
+          VAR_INDICES($$) = $indices;
+          AddLocToNode($$, &@1, &@1);
+        }
+      | ID[id]
+        {
+          $$ = ASTvar($id);
+          AddLocToNode($$, &@1, &@1);
+        }
+        ;
 
-expr: constant
+exprs: expr exprs { $$ = ASTexprs($1, $2); }
+     | expr COMMA exprs { $$ = ASTexprs($1, $3); }
+     | expr { $$ = ASTexprs($1, NULL); }
+     ;
+
+expr: constants
       {
         $$ = $1;
       }
-    | ID
+    | var
       {
-        $$ = ASTvar($1);
+        $$ = $1;
+      }
+    | funcall
+      {
+        $$ = $1;
       }
     | BRACKET_L expr BRACKET_R
       {
         $$ = $2;
       }
-    | BRACKET_L expr[left] binop[type] expr[right] BRACKET_R
+    | expr[left] binop[type] expr[right]
       {
         $$ = ASTbinop( $left, $right, $type);
         AddLocToNode($$, &@left, &@right);
       }
+    | expr[operand] monop[operator]
+      {
+        $$ = ASTmonop($operand, $operator);
+        AddLocToNode($$, &@operand, &@operator);
+      }
     ;
 
-constant: floatval
+constants: floatval
           {
             $$ = $1;
           }
@@ -138,6 +388,10 @@ boolval: TRUEVAL
          }
        ;
 
+monop: NOT       { $$ = MO_not; }
+    //  | MINUS     { $$ = MO_neg; }
+     ;
+
 binop: PLUS      { $$ = BO_add; }
      | MINUS     { $$ = BO_sub; }
      | STAR      { $$ = BO_mul; }
@@ -151,6 +405,12 @@ binop: PLUS      { $$ = BO_add; }
      | OR        { $$ = BO_or; }
      | AND       { $$ = BO_and; }
      ;
+
+type: INTTYPE   { $$ = CT_int; }
+    | FLOATTYPE { $$ = CT_float; }
+    | BOOLTYPE  { $$ = CT_bool; }
+    | VOIDTYPE  { $$ = CT_void; }
+    ;
 
 %%
 
