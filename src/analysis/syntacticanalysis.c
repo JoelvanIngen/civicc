1d5db7ae8daf4b7c4b4df51da6cbbcd91c3c6e84
Joel van Ingen <j.c.ingen@gmail.com>
2025-03-02 20:33:43 +0100

    Add changes needed for syntactical analysis milestone

---
+++ src/analysis/syntacticanalysis.c
@@ -0,0 +1,274 @@
+/**
+ * @file
+ *
+ * Traversal: SyntacticAnalysis
+ * UID      : SAN
+ *
+ *
+ */
+
+#include "ccn/ccn.h"
+#include "ccngen/ast.h"
+#include "ccngen/trav.h"
+
+/**
+ * @fn SANprogram
+ */
+node_st *SANprogram(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANdecls
+ */
+node_st *SANdecls(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANexprs
+ */
+node_st *SANexprs(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANarrexpr
+ */
+node_st *SANarrexpr(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANids
+ */
+node_st *SANids(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANexprstmt
+ */
+node_st *SANexprstmt(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANreturn
+ */
+node_st *SANreturn(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfuncall
+ */
+node_st *SANfuncall(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANcast
+ */
+node_st *SANcast(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfundefs
+ */
+node_st *SANfundefs(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfundef
+ */
+node_st *SANfundef(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfunbody
+ */
+node_st *SANfunbody(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANifelse
+ */
+node_st *SANifelse(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANwhile
+ */
+node_st *SANwhile(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANdowhile
+ */
+node_st *SANdowhile(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfor
+ */
+node_st *SANfor(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANglobdecl
+ */
+node_st *SANglobdecl(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANglobdef
+ */
+node_st *SANglobdef(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANparam
+ */
+node_st *SANparam(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANvardecl
+ */
+node_st *SANvardecl(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANstmts
+ */
+node_st *SANstmts(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANassign
+ */
+node_st *SANassign(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANbinop
+ */
+node_st *SANbinop(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANmonop
+ */
+node_st *SANmonop(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANvarlet
+ */
+node_st *SANvarlet(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANvar
+ */
+node_st *SANvar(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANnum
+ */
+node_st *SANnum(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANfloat
+ */
+node_st *SANfloat(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+/**
+ * @fn SANbool
+ */
+node_st *SANbool(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
