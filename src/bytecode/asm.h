441367c8ee5efab6ae9f0eb368809a1a5176c52c
Joel van Ingen <j.c.ingen@gmail.com>
2025-03-18 13:05:40 +0100

    Fix non-matching function header and declaration

--- src/bytecode/asm.h
+++ src/bytecode/asm.h
@@ -49,4 +49,4 @@ void ASMinit(Assembly* assembly);
 void ASMfree(Assembly** assembly_ptr);
 void ASMemitInstr(Assembly* assembly, char* instr_name, char* arg0, char* arg1, char* arg2, bool is_label);
 void ASMemitConst(Assembly* assembly, char* type, char* val);
-void ASMemitFunExport(Assembly* assembly, char name, char* type, char** args, bool is_main);
+void ASMemitFunExport(Assembly* assembly, char* name, char* type, char** args, bool is_main);
